d1a09bf9e55908057f42a0f4b49c425aa60aadb2
Rakib-spickolab <rakib@spickolab.net>
2022-09-22 20:30:42 +0600

    up

--- controller/SslController.js
+++ controller/SslController.js
@@ -6,7 +6,7 @@ exports.initSSL = async (req,res) => {
         total_amount: req.body.amount,
         currency: 'BDT',
         tran_id: 'uniqe',
-        success_url: 'https://pgw-user-interface-czqq-wuj712j8r-rakib-hasan14.vercel.app/',
+        success_url: 'http://192.168.0.104:3000/',
         fail_url: 'https://pgw-user-interface-czqq-wuj712j8r-rakib-hasan14.vercel.app/fail/',
         cancel_url: 'https://pgw-user-interface-czqq-wuj712j8r-rakib-hasan14.vercel.app/fail/',
         ipn_url: 'https://pgw-user-interface-czqq-wuj712j8r-rakib-hasan14.vercel.app/',
